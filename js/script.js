bc3ae13c28138255c09473e8666dea4e97a30d0d
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2022-09-06 18:34:20 -0300

    Updated

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ function scrolltop(){
         behavior: 'smooth'
     });
 }
+const menu_mobile = document.querySelector('.menu_mobile');
+const navigation = document.querySelector('.navigation');
+menu_mobile.onclick = function(){
+    navigation.classList.toggle('active');
+    menu_mobile.classList.toggle('active');
+   }
